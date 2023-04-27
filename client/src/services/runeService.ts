17516008d6074fe8d54d2807dc996b37e8ea9c7d
carlsylvan <carlsylvan@gmail.com>
2023-04-27 09:40:27 +0200

    axios runeservice

---
+++ client/src/services/runeService.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { IRune } from "../models/IRune";
+
+export const URL = "http://localhost:5000";
+
+export const getRunes = async () => {
+    let response = await axios.get<IRune[]>(
+        URL + '/runes'
+    );
+    return response.data;
+};
+
+export const getRuneById = async (id: number) => {
+    let response = await axios.get<IRune>(
+        URL + '/runes/' + id
+    );
+    return response.data;
+};
+
+export const writeRune = async (rune: IRune) => {
+    let response = await axios.post<IRune>(
+        URL + '/runes',
+        rune
+    );
+    return response.data;
+}
+
+export const deleteRune = async (id: number) => {
+    let response = await axios.delete<IRune>(
+        URL + '/runes/' + id
+    );
+    return response.data;
+};
