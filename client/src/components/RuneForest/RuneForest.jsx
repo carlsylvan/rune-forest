234fb34685220b9afcad50be6871735440440741
carlsylvan <carlsylvan@gmail.com>
2023-09-18 08:50:02 +0200

    RuneForest and RuneStone components

---
+++ client/src/components/RuneForest/RuneForest.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "/RuneForest.css";
+
+export const RuneForest = () => {
+  const [runes, setRunes] = useState([]);
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newRune = {
+      text,
+      author,
+    };
+    writeRune(newRune).then((rune) => {
+      setText("");
+      setAuthor("");
+    });
+
+    const deleteClick = (e) => {
+      e.preventDefault();
+      const id = e.currentTarget.id;
+      deleteRuneById(parseInt(id)).then((rune) => {
+        console.log(rune);
+      });
+    };
+
+    useEffect(() => {
+      getRunes().then((runes) => {
+        setRunes(runes);
+        console.log(runes);
+      });
+    });
+
+    return (
+      <div>
+        <h1>Rune Forest</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Write your rune here"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Write your name here"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <button type="submit">Write</button>
+        </form>
+
+        {runes.map((rune) => (
+          <Runestone key={rune.id}>
+            <h2>{rune.text}</h2>
+            <h3>{rune.author}</h3>
+            <button onClick={deleteClick}>Delete</button>
+          </Runestone>
+        ))}
+      </div>
+    );
+  };
+};
---
+++ client/src/components/RuneStone/RuneStone.jsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+
+export const RuneStone = (props) => {
+  return <div>{props.children}</div>;
+};
